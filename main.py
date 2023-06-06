39082b8be8f3cab1aa93816a21748ba6da8f32dd
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-06 23:37:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("100 Days of Code QUIZ")
+print()
+print("How many can you answer correctly?")
+ans1 = ("What language are we writting in?")
+if ans1 == "python" or ans1 == "Python":
+  print("Correct")
+else:
+  print("Nope🙈")
+ans2 = int(input("Which lesson number is this?"))
+if ans2 > 12:
+  print("We're not quite that far yet")
+elif ans2 < 12:
+  print("We've gone well past that!")
+else:
+  ans2 == 12
+  print("That's right!")
